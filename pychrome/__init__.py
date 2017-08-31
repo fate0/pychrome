919cb4f01e0774acc073a47b1f005c7a2050f065
fate0 <fate0@fatezero.org>
2017-08-31 19:06:02 +0800

    use local imports

--- pychrome/__init__.py
+++ pychrome/__init__.py
@@ -3,8 +3,8 @@
 
 from __future__ import unicode_literals
 
-from pychrome.browser import *
-from pychrome.tab import *
-from pychrome.exceptions import *
+from .browser import *
+from .tab import *
+from .exceptions import *
 
 __version__ = '0.2.0'
